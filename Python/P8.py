4ec63405faf55bec64b14a52bc0c371c7d93d64d
dimakiss <40802862+dimakiss@users.noreply.github.com>
2020-05-03 02:58:32 +0300

    Create P8.py

---
+++ Python/P8.py
@@ -0,0 +1,35 @@
+
+# Solution to problem 7
+# https://projecteuler.net/problem=7
+# Run time  1.87ms
+
+#This solution is to take the initial 13 dig number and add the next number and erase the last number
+#It's more efficient then multiplying every time 13 digs
+
+#read file with numbers
+f = open("numbers.txt", "r")
+numbers=f.read()
+temp_num=1
+
+zf=0 #kind of a register if there 0 in the sequence if it's don't calculate the product because is supposed to be 0
+for i in range(13):
+    if int(numbers[i])==0 :
+        zf+=1
+    else:
+        temp_num *= int(numbers[i])
+print(zf)
+max_prod=0
+
+for i in range(0,numbers.__len__()-15):
+    if zf == 0:
+        max_prod = max(max_prod, temp_num)
+    if numbers[i + 13] == "0":
+        zf += 1
+    else:
+        temp_num *= int(numbers[i + 13])
+    if numbers[i] == "0":
+        zf -= 1
+    else:
+        temp_num //= int(numbers[i])
+print(max_prod)
+# Answer: 23514624000
