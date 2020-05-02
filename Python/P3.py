451e8c9bce70a901b5431ac610c38a196dba8a5b
dimakiss <40802862+dimakiss@users.noreply.github.com>
2020-05-03 01:41:04 +0300

    Update P3.py

--- Python/P3.py
+++ Python/P3.py
@@ -3,12 +3,12 @@
 # https://projecteuler.net/problem=3
 # Run time less then 1ms
 
-#To solve this problem we will mention that every number can ber represented by its prime factor
+#To solve this problem we will mention that every number can be represented by its prime factor
 #for example 81=3^4 ; 85=17*5 ;1050=2*3*5^2 *7
-#the first number the divide 1050 for example is 2 we will dived 1050 by to and we will get 525
-#525 devided by 3 we will get 175 which devided by 5 twice and we left with 7 whitch indeed the largest prime factor
-
-# calculate get n as number
+#the first number the divide 1050, for example, is 2 we will be divided 1050 by to and we will get 525
+#525 divided by 3 we will get 175 which divided by 5 twice and we left with 7 which indeed the largest prime factor
+ 
+# calculate get n as the number
 # calculate return the Largest prime factor
 def calculate(n):
         p=2
@@ -19,8 +19,10 @@ def calculate(n):
             n //=p
         return 0
 
-# smallest_prime_factor get n as number and k as last know number that divide n (by default this numbeer as smallest prime is 2)
-# smallest_prime_factor return the smallest number that divide n
+# smallest_prime_factor get n as number and k as last know number that divides n 
+#(by default this number as smallest prime is 2)
+#
+# smallest_prime_factor return the smallest number that divides n
 def smallest_prime_factor(n,k):
     for i in range(k, (int)(n**0.5) + 1):
         if n % i == 0:
