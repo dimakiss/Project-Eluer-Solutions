37cb514f72f9c6f45579c2f50771a4866f473489
dimakiss <40802862+dimakiss@users.noreply.github.com>
2020-05-04 20:30:44 +0300

    Create P030.py

---
+++ Python/P030.py
@@ -0,0 +1,21 @@
+
+# Solution to problem 30
+# https://projecteuler.net/problem=30
+# Run time  1381.76 ms
+
+sum=0
+# we can predict that the highest number is 325836 --> maximu diget is 9 so 9^5*n where n is the numbers length
+# so 9^5 *n should always be grater then 10^n or even equal to is for our problen
+# so when 10^n is alway equal we have a problen
+# when 9^5 *n -10^n <0 its to upper limit or n=5.513 so the upper limit is 10^5.513 or 325836.701->325836
+for i in range(10000,325836):
+    temp=i
+    temp_sum=0
+    while temp>0:
+        temp_sum+=(temp%10)**5
+        temp//=10
+    if temp_sum==i:
+        sum+=temp_sum
+        print(i)
+print(sum)
+# Answer: 435538
