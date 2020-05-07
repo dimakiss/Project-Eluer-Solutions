476299533832e47873752badc6f38d6ca52c8e87
dimakiss <40802862+dimakiss@users.noreply.github.com>
2020-05-08 02:18:51 +0300

    Update P030.py

--- Python/P030.py
+++ Python/P030.py
@@ -8,7 +8,7 @@ sum=0
 # so 9^5 *n should always be grater then 10^n or even equal to is for our problen
 # so when 10^n is alway equal we have a problen
 # when 9^5 *n -10^n <0 its to upper limit or n=5.513 so the upper limit is 10^5.513 or 325836.701->325836
-for i in range2,325836):
+for i in range(2,325836):
     temp=i
     temp_sum=0
     while temp>0:
