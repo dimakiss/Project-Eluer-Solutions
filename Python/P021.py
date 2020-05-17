5e3c11015ed258fc0619bd9ee89dd9091cf4f741
dimakiss <40802862+dimakiss@users.noreply.github.com>
2020-05-18 02:31:45 +0300

    Create P021.py

---
+++ Python/P021.py
@@ -0,0 +1,24 @@
+
+#Solution to problem 21
+#https://projecteuler.net/problem=21
+#Run time 4974.01ms
+
+#divisitors get num as the number
+#divisitors return the sum of divisors of num
+def divisitors_sum(num):
+    sum=1
+    sqrt=int(num**0.5)
+    for i in range(2,sqrt):
+        if num%i==0:
+            sum+=i+num//i
+    if int(sqrt**2)==num:
+        sum+=int(sqrt)
+    return sum
+
+sum=0
+for i in range(3,100001):
+    temp=divisitors_sum(i)
+    if divisitors_sum(temp)==i and temp!=i:
+        sum+=i+temp
+print(sum//2) ## its take twice every option for example in i==220  d(220) = 284 it will tkae 220 284 and for i==284 the same
+#Answer: 852810
