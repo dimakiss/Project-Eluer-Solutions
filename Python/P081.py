d4c942c8502eb1888d273a3e5dfe4bf72e09c4dc
dimakiss <40802862+dimakiss@users.noreply.github.com>
2020-06-25 00:11:30 +0300

    Create P081.py

---
+++ Python/P081.py
@@ -0,0 +1,28 @@
+
+# Solution to problem 81 
+# https://projecteuler.net/problem=81
+# Run time 1089 ms
+
+#lst=[[131,673,234,103,18],[201,96,342,965,150],[630,803,746,422,111],[637,699,497,121,956],[805,732,524,37,331]]
+lst=[]
+
+with open('matrix.txt','r') as f:
+    buff=f.read()
+for i in buff.split('\n'):
+    temp=[]
+    for j in i.split(','):
+        temp.append(int(j))
+    lst.append(temp)
+
+# initiating the lowest and rightest position
+for i in range(lst.__len__()-2,-1,-1):
+    lst[i][lst.__len__()-1]+=lst[i+1][lst.__len__()-1]
+for i in range(lst.__len__()-2,-1,-1):
+    lst[lst.__len__()-1][i]+=lst[lst.__len__()-1][i+1]
+
+# by going from the end, in every position, there is an option to go right or down
+for i in range(lst.__len__()-2, -1, -1):
+    for j in range(lst.__len__()-2, -1, -1):
+           lst[i][j]+=min(lst[i][j+1],lst[i+1][j])
+print(lst[0][0])
+#Answer: 427337
